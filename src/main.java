6238ad3198c4739cbff4cb647ac6d0ac230681b7
Bruno Rolemberg de Albuquerque <bruno.2010478300026@faeterj-rio.edu.br>
2024-05-14 14:04:32 -0300

    Primeiro commit

---
+++ src/main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+        gerenciadorProdutos gerenciador = new gerenciadorProdutos();
+
+        gerenciador.processa(scanner);
+
+        scanner.close();
+	}
+
+}
